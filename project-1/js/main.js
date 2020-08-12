0def5c089b533d21603f548ddb556fc2dad12b53
jahangeershah <jahangirshah2010@gmail.com>
2020-08-12 13:34:40 -0700

    javascript file updated with username

--- project-1/js/main.js
+++ project-1/js/main.js
@@ -9,6 +9,9 @@ const Password2 = document.getElementById('Confirm-Password');
 function ShowError(input,message){
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
+    small = formControl.querySelector('small');
+    small.innerText = message;
+    
 }
 function showSuccess(input,message){
     const formControl = input.parentElement;
@@ -19,22 +22,22 @@ function showSuccess(input,message){
 form.addEventListener('submit',function(e){
     e.preventDefault();
     if(Username.value == ''){
-        ShowError(Username,'username required');
+        ShowError(Username,'Username required');
     }else{
         showSuccess(Username)
     }
     if(Email.value == ''){
-        ShowError(Email,'username required');
+        ShowError(Email,'Email required');
     }else{
         showSuccess(Email)
     }
     if(Password.value == ''){
-        ShowError(Password,'username required');
+        ShowError(Password,'Password  required');
     }else{
         showSuccess(Password)
     }
     if(Password2.value == ''){
-        ShowError(Password2,'username required');
+        ShowError(Password2,'Password required');
     }else{
         showSuccess(Password2)
     }
