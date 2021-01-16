800710d5384eca6fcb5ed0639dcc78d4f9f32400
jahangeer shah <jahangirshah2010@gmail.com>
2021-01-16 16:22:01 +0500

    javascript file created with some controller and avent listners

---
+++ final-project/js/main.js
@@ -0,0 +1,41 @@
+// stoage controller
+
+
+// Item controller
+const ItemCtrl = (function() {
+    // Item constructor
+    const Item = function(id, name, caloires) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+
+    }
+
+})();
+
+// Data Structure / State 
+const data = {
+    items: [
+        { id: 0, name: 'steak Dinner', Calories: 1200 },
+        { id: 2, name: 'Cookie', calories: 1200 },
+        { id: 3, name: 'Eggs', Calories: 1200 }
+    ],
+    currentItem: null,
+    totalCalories: 0
+}
+return {
+    logData: function() {
+        return data;
+    }
+}
+
+// UI Controller
+const UICtrl = (function() {
+
+})();
+
+// Ap Controller
+
+const App = (function() {
+    const
+})();
