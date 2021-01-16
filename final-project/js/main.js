6736b9c66498cb05f96f09a09464c20f37af5aa9
jahangeer shah <jahangirshah2010@gmail.com>
2021-01-16 16:50:23 +0500

    data controller added

--- final-project/js/main.js
+++ final-project/js/main.js
@@ -11,31 +11,51 @@ const ItemCtrl = (function() {
 
     }
 
+    // Data Structure / State 
+    const data = {
+            items: [
+                { id: 0, name: 'steak Dinner', Calories: 1200 },
+                { id: 2, name: 'Cookie', calories: 1200 },
+                { id: 3, name: 'Eggs', Calories: 1200 }
+            ],
+            currentItem: null,
+            totalCalories: 0
+        }
+        // Public Methods
+    return {
+        logData: function() {
+            return data;
+        }
+    }
+
 })();
 
-// Data Structure / State 
-const data = {
-    items: [
-        { id: 0, name: 'steak Dinner', Calories: 1200 },
-        { id: 2, name: 'Cookie', calories: 1200 },
-        { id: 3, name: 'Eggs', Calories: 1200 }
-    ],
-    currentItem: null,
-    totalCalories: 0
-}
-return {
-    logData: function() {
-        return data;
-    }
-}
+
 
 // UI Controller
 const UICtrl = (function() {
+    // Public Methods
+    return {
 
+    }
 })();
 
 // Ap Controller
 
-const App = (function() {
-    const
-})();
+const App = (function(ItemCtr, UICtrl) {
+
+    // console.log(ItemCtrl.logData())
+
+    //Public methods
+    return {
+        init: function() {
+            console.log('initializing App...');
+        }
+    }
+
+})(ItemCtrl, UICtrl);
+
+
+// Initializing app
+
+App.init();
